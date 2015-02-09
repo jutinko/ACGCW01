5edb4d218210b8b2c34405e70b908dde21d9d5c8
jutinko <jutkko@gmail.com>
2015-02-09 16:48:49 +0000

    REtabbed

--- ExampleCode/main.cpp
+++ ExampleCode/main.cpp
@@ -34,102 +34,102 @@ float w(float x)
 void applyFunctionOnAllPixelsPFM(vector<float*> images_in, float* image_out, 
     uint width, uint height, uint numComponents, float (*func)(vector<float*>, uint))
 {
-	for ( uint i = 0 ; i < height ; ++i ) // height
+  for ( uint i = 0 ; i < height ; ++i ) // height
   {
-		for ( uint j = 0 ; j < width ; ++j ) // width
-		{
-			for ( uint k = 0 ; k < numComponents ; ++k ) // color channels - 3 for RGB images
-			{
-				uint index = i*width*numComponents + j*numComponents + k; //index within the image
+    for ( uint j = 0 ; j < width ; ++j ) // width
+    {
+      for ( uint k = 0 ; k < numComponents ; ++k ) // color channels - 3 for RGB images
+      {
+        uint index = i*width*numComponents + j*numComponents + k; //index within the image
         image_out[index] = func(images_in, index);
-			}
-		}
-	}
+      }
+    }
+  }
 }
 
 void applyFunctionOnAllPixelsPPM(vector<unsigned char*> images_in, unsigned char* image_out, 
     uint width, uint height, uint numComponents, 
     char (*func)(vector<unsigned char*>, uint))
 {
-	for ( uint i = 0 ; i < height ; ++i ) // height
+  for ( uint i = 0 ; i < height ; ++i ) // height
   {
-		for ( uint j = 0 ; j < width ; ++j ) // width
-		{
-			for ( uint k = 0 ; k < numComponents ; ++k ) // color channels - 3 for RGB images
-			{
-				uint index = i*width*numComponents + j*numComponents + k; //index within the image
+    for ( uint j = 0 ; j < width ; ++j ) // width
+    {
+      for ( uint k = 0 ; k < numComponents ; ++k ) // color channels - 3 for RGB images
+      {
+        uint index = i*width*numComponents + j*numComponents + k; //index within the image
         image_out[index] = func(images_in, index);
-			}
-		}
-	}
+      }
+    }
+  }
 }
 
 void LoadPPMAndSavePFM(const char *image_in, const char *image_out)
 {
-	unsigned char *img_in = loadPNM(image_in, width, height, numComponents);
-	float *img_out = new float [width*height*numComponents];
+  unsigned char *img_in = loadPNM(image_in, width, height, numComponents);
+  float *img_out = new float [width*height*numComponents];
 
-	for ( uint i = 0 ; i < height ; ++i ) // height
+  for ( uint i = 0 ; i < height ; ++i ) // height
   {
-		for ( uint j = 0 ; j < width ; ++j ) // width
-		{
-			for ( uint k = 0 ; k < numComponents ; ++k ) // color channels - 3 for RGB images
-			{
-				uint index = i*width*numComponents + j*numComponents + k; //index within the image
+    for ( uint j = 0 ; j < width ; ++j ) // width
+    {
+      for ( uint k = 0 ; k < numComponents ; ++k ) // color channels - 3 for RGB images
+      {
+        uint index = i*width*numComponents + j*numComponents + k; //index within the image
 
-				//typecast 0 - 255 values to the 0.0f -> 1.0f range 
-				img_out[index] = static_cast<float>(img_in[index])/255.0f; //typecast all color channels of each pixel
-								
-			}
-		}
-	}
-	WritePFM(image_out, width, height, numComponents, img_out);
+        //typecast 0 - 255 values to the 0.0f -> 1.0f range 
+        img_out[index] = static_cast<float>(img_in[index])/255.0f; //typecast all color channels of each pixel
+                
+      }
+    }
+  }
+  WritePFM(image_out, width, height, numComponents, img_out);
 }
 
 void LoadPFMAndSavePPM(const char *image_in, const char *image_out)
 {
-	float *img_in = loadPFM(image_in, width, height, numComponents);
-	unsigned char *img_out = new unsigned char [width*height*numComponents];
+  float *img_in = loadPFM(image_in, width, height, numComponents);
+  unsigned char *img_out = new unsigned char [width*height*numComponents];
 
-	for ( uint i = 0 ; i < height ; ++i ) // height
+  for ( uint i = 0 ; i < height ; ++i ) // height
   {
-		for ( uint j = 0 ; j < width ; ++j ) // width
-		{
-			for ( uint k = 0 ; k < numComponents ; ++k ) // color channels - 3 for RGB images
-			{
-				uint index = i*width*numComponents + j*numComponents + k; //index within the image
+    for ( uint j = 0 ; j < width ; ++j ) // width
+    {
+      for ( uint k = 0 ; k < numComponents ; ++k ) // color channels - 3 for RGB images
+      {
+        uint index = i*width*numComponents + j*numComponents + k; //index within the image
 
-				//typecast 0.0f -> 1.0f values to the 0 - 255 range 
-				img_out[index] = static_cast<unsigned char>(img_in[index]*255.0f); //typecast all color channels of each pixel
-								
-			}
-		}
-	}
-	WritePNM(image_out, width, height, numComponents, img_out);
+        //typecast 0.0f -> 1.0f values to the 0 - 255 range 
+        img_out[index] = static_cast<unsigned char>(img_in[index]*255.0f); //typecast all color channels of each pixel
+                
+      }
+    }
+  }
+  WritePNM(image_out, width, height, numComponents, img_out);
 }
 
 void CreateAndSavePFM(const char *image_out)
 {
-	width = 511; // set size of image to 511x511 pixels
-	height = 511;
-	numComponents = 3;
-	
-	float *img_out = new float [width*height*numComponents];
+  width = 511; // set size of image to 511x511 pixels
+  height = 511;
+  numComponents = 3;
+  
+  float *img_out = new float [width*height*numComponents];
 
-	for ( uint i = 0 ; i < height ; ++i ) // height
+  for ( uint i = 0 ; i < height ; ++i ) // height
   {
-		for ( uint j = 0 ; j < width ; ++j ) // width
-		{
-			for ( uint k = 0 ; k < numComponents ; ++k ) // color channels - 3 for RGB images
-			{
-				uint index = i*width*numComponents + j*numComponents + k; //index within the image
+    for ( uint j = 0 ; j < width ; ++j ) // width
+    {
+      for ( uint k = 0 ; k < numComponents ; ++k ) // color channels - 3 for RGB images
+      {
+        uint index = i*width*numComponents + j*numComponents + k; //index within the image
 
-				//set image to white
-				img_out[index] = 1.0f; //RGB all set to white
-			}
-		}
-	}
-	WritePFM(image_out, width, height, numComponents, img_out);
+        //set image to white
+        img_out[index] = 1.0f; //RGB all set to white
+      }
+    }
+  }
+  WritePFM(image_out, width, height, numComponents, img_out);
 }
 
 char returnSameValuePPM(vector<unsigned char*> imgs_in, uint index)
@@ -139,12 +139,12 @@ char returnSameValuePPM(vector<unsigned char*> imgs_in, uint index)
 
 void LoadAndSavePPM(const char *image_in, const char *image_out)
 {
-	unsigned char *img_in = loadPNM(image_in, width, height, numComponents);
-	unsigned char *img_out = new unsigned char [width*height*numComponents];
+  unsigned char *img_in = loadPNM(image_in, width, height, numComponents);
+  unsigned char *img_out = new unsigned char [width*height*numComponents];
   vector<unsigned char*> imgs_in;
   imgs_in.push_back(img_in);
   applyFunctionOnAllPixelsPPM(imgs_in, img_out, width, height, numComponents, returnSameValuePPM);
-	WritePNM(image_out, width, height, numComponents, img_out);
+  WritePNM(image_out, width, height, numComponents, img_out);
   delete img_out;
 }
 
@@ -190,13 +190,9 @@ void LoadAndSavePFM(vector<const char*> images_in, const char *image_out)
   {
     imgs_in.push_back(loadPFM(*i, width, height, numComponents));
   }
-	//float *img_in = loadPFM(image_in, width, height, numComponents);
-	float *img_out = new float [width*height*numComponents];
-  //imgs_in.push_back(img_in);
-  //applyFunctionOnAllPixelsPFM(imgs_in, img_out, width, height, numComponents, returnSameValuePFM);
-      cout << "befeore apply fdsjak;e" << endl;
+  float *img_out = new float [width*height*numComponents];
   applyFunctionOnAllPixelsPFM(imgs_in, img_out, width, height, numComponents, returnHDRCoponentPFM);
-	WritePFM(image_out, width, height, numComponents, img_out);
+  WritePFM(image_out, width, height, numComponents, img_out);
   delete img_out;
 }
 
